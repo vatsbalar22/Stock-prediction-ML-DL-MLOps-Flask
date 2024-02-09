3ae471a71bff05395561a0c5bf956fb037a2e933
Vats Balar <141894006+vatsbalar22@users.noreply.github.com>
2024-02-09 11:07:23 +0000

    adding cli on model

--- train_model.py
+++ train_model.py
@@ -1,6 +1,5 @@
 # train_model.py
 
-import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
